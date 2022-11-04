7fed0ae61a46201c057097eaab5de64714931e12
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-04 09:59:13 +0100

    aggiunta commenti js

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ let maxNumber = 100;
 startGame(difficult);
 
 function startGame(difficult){
+    // svuotiamo il contenitore delle celle
     playBoard.innerHTML = "";
+    // creo la variabile che terrà il punteggio della partita inizializzandolo a 0 e la stampo in uno span
     let punteggio = 0;
     points.innerHTML = punteggio;
+    // creo una variabile che verifica se abbiamo già perso
     let sconfitta = false;
 
+    // verifico la difficoltà inserita, all'inizio è inizializzata a easy(100 celle)
     if(difficult === "easy"){
         maxNumber = 100;
     }else if(difficult === "medium"){
@@ -22,19 +26,21 @@ function startGame(difficult){
     }else{
         maxNumber = 49;
     }
+    // creo l'array contenente le posizioni delle bombe
     const bombs = bombCreation(maxNumber);
     let numSelected = [];
 
+    // popolo la board con tutti i numeri fino a 100/81/49 a seconda della difficoltà
     for (let i = 1; i <= maxNumber; i++) {
         const playNumber = document.createElement("div");
         playNumber.innerHTML = i;
         playNumber.classList.add('board__number');
-
+        // creo l'evento click delle celle verificando da prima se abbiamo già preso una bomba e nel caso sia vero rimuovo l'eventlistener
         playNumber.addEventListener("click", function(){
-                // this.removeEventListener("click", function(){});
             if(sconfitta === true){
                 playNumber.removeEventListener("click", function(){});
             }else{
+                // verifico se la casella cliccata è una bomba se è vero applico la classe bomba e tramite una funzione applico la stessa classe a tutte le bombe, cambio la variabile sconfitta in true in modo che vengano rimossi poi tutti gli EventListner
                 if(bombs.includes(i)){
                     this.classList.add('bomb');
                     alert(`Hai perso con un punteggio di: ${punteggio}`);
@@ -44,6 +50,7 @@ function startGame(difficult){
                     if(playNumber.classList.contains('selected')){
                         playNumber.removeEventListener("click", function(){});
                     }else{
+                        // verico se la casella non è già stata cliccata e in caso aumento il punteggio e aggiungo la classe numSelected, inoltre verifico se si ha vinto facendo numero celle totali - numero bombCreation, se il punteggio è uguale si vince
                         punteggio += 1;
                         points.innerHTML = punteggio;
                         this.classList.add('selected');
@@ -56,6 +63,7 @@ function startGame(difficult){
             }  
         });
 
+        // a seconda della difficoltà applico una larghezza definita alle celle in modo che occupino in ogni caso la nostra board che è 500px x 500px
         if(difficult === "easy"){
             playNumber.style.width = "calc(100%/10)";
             playNumber.style.height = "calc(100%/10)";
@@ -71,11 +79,13 @@ function startGame(difficult){
     console.log(bombs);
 }
 
+// creo un event che fa partire una nuova partita in base alla difficoltà scelta
 playButton.addEventListener("click", function(){
     difficult = playDifficult.value;
     startGame(difficult);
 });
 
+// funzione che crea l'array con le bombe
 function bombCreation(maxNumber){
     let bombPosition = [];
     while(bombPosition.length < 16){
@@ -87,11 +97,13 @@ function bombCreation(maxNumber){
     return bombPosition;
 }
 
+// funzione che genera un numero casuale da 1 a numMax
 function msMathRandom(nMax){
     const result = Math.floor(Math.random() * nMax) + 1;
     return result;
 }
 
+// funzione che applica la classe bomb a tutte le celle conmtenute nell'array delle bombe
 function loseGame(bombsArray){
     for (let i = 0; i < bombsArray.length; i++) {
         const element = bombsArray[i];
